568223e308d889a30d61c6486136894d4f2620ee
Gonçalo Nuno Bernardo <up201606058@fe.up.pt>
2020-11-30 13:39:31 +0000

    Add simple initial menu

--- memgame/sketch.js
+++ memgame/sketch.js
@@ -4,6 +4,7 @@ var round_finished_counter_set = false;
 
 let song, right_answer, wrong_answer;
 let polySynthesizer = new p5.PolySynth();
+let clicked = false;
 
 function preload() {
   song = loadSound('assets/DivineMetatron.mp3');
@@ -19,37 +20,49 @@ function setup() {
 
   song.loop();
   song.play();
+
+
 }
 
 function draw() {
   background(220);
 
+    if (!clicked) {
+      textAlign(CENTER, 150);textSize(32);fill(color(0,0,0));text('VediSonor Memory Game', 200, 150);
+      textAlign(CENTER, CENTER);textSize(16);fill(color(0,0,0));text('Click to Start', 200, 200);
+    }
+
   // leftkey or say "yes" - its a cosoco
   // rightkey or say "no" - its not a cosoco
+  if (clicked){
+    if (!game.game_finished) {
+      game.play();
+    } else {
 
-  if (!game.game_finished) {
-    game.play();
-  } else {
+      if (!round_finished_counter_set) {
+        game.counter = millis();
+        round_finished_counter_set = true;
 
-    if (!round_finished_counter_set) {
-      game.counter = millis();
-      round_finished_counter_set = true;
+      }
 
+      fill(color(0, 0, 0));
+      textSize(32);
+      text("Round finished!", 100, 200);
+
+      textSize(16);
+      text("Number of cosocos: " + game.n_cosocos + ".", 100, 220);
+
+      if (millis() >= 3000 + game.counter) {
+        n_cosocos++;
+        game = new Memgame(n_cosocos, polySynthesizer, right_answer, wrong_answer);
+        round_finished_counter_set = false;
+      }
     }
-
-    fill(color(0, 0, 0));
-    textSize(32);
-    text("Round finished!", 100, 200);
-
-    textSize(16);
-    text("Number of cososcos: " + game.n_cosocos + ".", 100, 220);
-
-    if (millis() >= 3000 + game.counter) {
-      n_cosocos++;
-      game = new Memgame(n_cosocos, polySynthesizer, right_answer, wrong_answer);
-      round_finished_counter_set = false;
-    }
-
   }
+}
 
+function mouseClicked() {
+  if (!clicked) {
+    clicked = true;
+  }
 }

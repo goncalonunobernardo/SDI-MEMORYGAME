2e8953f5fe28d71e803e879d5586b73e4eefa3a7
MartimChaves <43721946+MartimChaves@users.noreply.github.com>
2020-12-01 17:24:52 +0000

    Colours reasonably separated, repeat/play option, intro text

--- memgame/memgame.js
+++ memgame/memgame.js
@@ -7,7 +7,7 @@ class Memgame {
   //TODO: Create visuals instead of squares.
 
   constructor(num_cosocos, polysynth, right_answer, wrong_answer) {
-    this.sorted_pitches= shuffle(["A3","C4","D4","E4","G4","A4","C5","D5","E5","G5","A5","C6"]);// A minor pentatonic scale
+    this.sorted_pitches = shuffle(["A3", "C4", "D4", "E4", "G4", "A4", "C5", "D5", "E5", "G5", "A5", "C6"]); // A minor pentatonic scale
     this.polySynthesizer = polysynth;
     this.right_answer = right_answer;
     this.wrong_answer = wrong_answer;
@@ -16,9 +16,33 @@ class Memgame {
 
     this.n_cosocos = num_cosocos;
 
+    if (num_cosocos < 7) {
+      this.colour_thresh = 120;
+    } else if (num_cosocos > 7 && num_cosocos < 15) {
+      this.colour_thresh = 70;
+    } else if (num_cosocos > 15 && num_cosocos < 20) {
+      this.colour_thresh = 40;
+    } else {
+      this.colour_thresh = 20;
+    }
+
+    var color_not_diff;
+    var color_cosoco;
     for (let k = 0; k < num_cosocos; k++) {
+
+      color_not_diff = true;
+
+      if (k > 0) {
+        while (color_not_diff) {
+          color_cosoco = [random(255), random(255), random(255)];
+          color_not_diff = this.check_colours(color_cosoco);
+        }
+      } else {
+        color_cosoco = [random(255), random(255), random(255)];
+      }
+
       this.cosoco_dict[k] = [
-        [random(255), random(255), random(255)], this.sorted_pitches[k], 0.2
+        color_cosoco, this.sorted_pitches[k], 0.2
       ]
     }
 
@@ -29,6 +53,10 @@ class Memgame {
     this.intro_cosoco_counter = 0;
     this.show_intro_cosocos = false;
     this.intro_counter_set = false;
+
+    // between intro and testing - give option to repeat cosocos set
+    this.allow_repeat = false;
+    this.repeat_counter_set = false;
 
     // testing
     this.test_cosoco_counter = 0;
@@ -41,6 +69,8 @@ class Memgame {
     this.test_colour = 0;
     this.test_sound = 0;
     this.game_finished = false;
+    this.answers_started = false;
+    this.answer_eval = "";
 
     // Speech Rec
     this.continuous = false;
@@ -50,11 +80,22 @@ class Memgame {
 
   }
 
-  gotSpeech() {
-    if (this.speechRec.resultValue) {
-      this.wordHeard = this.speechRec.resultString;
+  check_colours(colour) {
+
+    for (let i = 0; i < Object.keys(this.cosoco_dict).length; i++) {
+      let diff_red = pow(this.cosoco_dict[i][0][0] - colour[0], 2);
+      let diff_green = pow(this.cosoco_dict[i][0][1] - colour[1], 2);
+      let diff_blue = pow(this.cosoco_dict[i][0][2] - colour[2], 2);
+
+      let diff_color = sqrt(diff_red + diff_green + diff_blue);
+
+      if (diff_color < this.colour_thresh) {
+        return true;
+      }
     }
+    return false;
   }
+
 
   introduce_cosocos() {
 
@@ -77,7 +118,11 @@ class Memgame {
 
       if (this.intro_cosoco_counter == this.n_cosocos) {
         this.intro_cosoco_counter++;
-        this.start_test_cosocos = true;
+
+        // allow repeat = true
+        this.allow_repeat = true;
+
+        //this.start_test_cosocos = true; //remove
       }
 
     }
@@ -86,8 +131,8 @@ class Memgame {
       fill(color(this.cosoco_dict[this.intro_cosoco_counter - 1][0]));
       noStroke();
       rect(100, 100, 200, 200);
-      textSize(32);
-      text(this.cosoco_dict[this.intro_cosoco_counter - 1][1], 150, 390);
+      //textSize(32);
+      //text(this.cosoco_dict[this.intro_cosoco_counter - 1][1], 150, 390);
 
       let midiNote = this.cosoco_dict[this.intro_cosoco_counter - 1][1];
       this.polySynthesizer.play(midiNote, 0.3, 0, 1);
@@ -104,6 +149,51 @@ class Memgame {
       this.introduce_cosocos();
     }
 
+    // allow repeat
+    if (this.allow_repeat) {
+
+      if (!this.repeat_counter_set) {
+        this.counter = millis();
+        this.repeat_counter_set = true;
+      }
+
+      if (!this.speechRecOn) {
+        this.speechRec = new p5.SpeechRec('en-US');
+        this.speechRec.start(this.continuous, this.interim);
+        this.speechRecOn = true;
+      }
+
+      if (this.speechRec.resultString == "repeat" || keyIsDown(LEFT_ARROW)) {
+        this.intro_cosoco_counter = 0;
+        this.show_intro_cosocos = false;
+        this.intro_counter_set = false;
+        this.allow_repeat = false;
+        this.speechRecOn = false;
+
+      } else if (this.speechRec.resultString == "play" || keyIsDown(RIGHT_ARROW)) {
+        this.start_test_cosocos = true;
+        this.allow_repeat = false;
+        this.speechRecOn = false;
+      }
+
+      textAlign(CENTER, CENTER);
+      fill(color(0, 0, 0));
+      textSize(32);
+      text("Say 'Repeat' if you wish\nto hear the set again.", 200, 100);
+      text("Say 'Play' if you wish\nto start the round.", 200, 250);
+      textAlign(CENTER, BOTTOM);
+      textSize(16);
+      text("Alternatively use Left key (<-) to repeat,\nRight key (->) to play", 200, 350);
+
+      // after some time reset speechRec 
+      if (millis() >= 5000 + this.counter) {
+        this.speechRecOn = false;
+        this.counter = millis();
+      }
+
+      //console.log(this.speechRec.resultString);
+    }
+
     // if testing
     if (this.start_test_cosocos && !this.game_finished) {
       this.test_user();
@@ -115,9 +205,20 @@ class Memgame {
 
     if (!this.test_counter_set) {
       this.counter = millis();
-      this.cosoco_dict_tbg = {...this.cosoco_dict};
+      this.cosoco_dict_tbg = {
+        ...this.cosoco_dict
+      };
       //Object.assign(this.cosoco_dict_tbg,this.cosoco_dict); // optional way to copy dicts
       this.test_counter_set = true;
+    }
+
+    if (!this.show_test_cosocos && this.answergiven && this.answers_started) {
+
+      textAlign(CENTER, CENTER);
+      fill(color(0, 0, 0));
+      textSize(24);
+      text(this.answer_eval, 200, 200)
+
     }
 
     if (millis() >= 3000 + this.counter && this.answergiven) { //answerGiven = true
@@ -138,7 +239,7 @@ class Memgame {
       } else {
         let other_cosocos = [...Object.keys(this.cosoco_dict)];
         if (other_cosocos.length > 1) {
-          other_cosocos.splice(this.test_colour,1);
+          other_cosocos.splice(this.test_colour, 1);
         }
         let diff_random_cosoco = Math.floor(Math.random() * other_cosocos.length);
         this.test_sound = other_cosocos[diff_random_cosoco];
@@ -148,6 +249,7 @@ class Memgame {
       this.counter = millis();
 
       this.answergiven = false;
+      this.answers_started = true;
 
     } else if (millis() >= 2000 + this.counter && !this.answergiven) {
 
@@ -159,8 +261,8 @@ class Memgame {
       fill(color(this.cosoco_dict[this.test_colour][0]));
       noStroke();
       rect(100, 100, 200, 200);
-      textSize(32);
-      text(this.cosoco_dict[this.test_sound][1], 150, 390);
+      //textSize(32);
+      //text(this.cosoco_dict[this.test_sound][1], 150, 390);
 
       this.polySynthesizer.play(this.cosoco_dict[this.test_sound][1], 0.3, 0, 0.01);
       console.log(this.cosoco_dict[this.test_sound][1]);
@@ -176,21 +278,40 @@ class Memgame {
         this.speechRecOn = true;
       }
 
-      if (this.speechRec.resultString == "yes" || keyIsDown(LEFT_ARROW)) {//yes its the cosoco ive seen before
+      if (this.speechRec.resultString == "yes" || keyIsDown(LEFT_ARROW)) { //yes its the cosoco ive seen before
         this.answergiven = true;
         this.counter = millis();
 
         // is testsound = testcolor -> correct, pop cosoco out of tbg
         this.checkAnswer("left");
         this.speechRecOn = false;
+        console.log(this.speechRec.resultString);
 
-      } else if (this.speechRec.resultString == "no" || keyIsDown(RIGHT_ARROW)) {//no, its not the cosoco ive seen before
+      } else if (this.speechRec.resultString == "no" || keyIsDown(RIGHT_ARROW)) { //no, its not the cosoco ive seen before
         this.answergiven = true;
         this.counter = millis();
 
         this.checkAnswer("right");
         this.speechRecOn = false;
+        console.log(this.speechRec.resultString);
+      }
 
+      // after some time reset speechRec 
+      if (millis() >= 6000 + this.counter) {
+        this.speechRecOn = false;
+        this.counter = millis();
+      }
+
+      // Instructions
+      if (!this.show_test_cosocos) {
+        textAlign(CENTER, CENTER);
+        fill(color(0, 0, 0));
+        textSize(32);
+        text("Say 'Yes' if you think\nthis is a cosoco.", 200, 100);
+        text("Say 'No' if you don't think\nthis is a cosoco.", 200, 250);
+        textAlign(CENTER, BOTTOM);
+        textSize(16);
+        text("Alternatively use Left key (<-) for 'Yes',\nand Right key (->) for 'No'", 200, 350);
       }
 
     }
@@ -201,6 +322,7 @@ class Memgame {
 
     if (this.test_colour == this.test_sound) {
       if (input == "left") {
+        this.answer_eval = "Correct, this really\nis a cosoco! Nice!";
         console.log("Correct, this really is a cosoco! Nice!");
         // remove from tbg
         delete this.cosoco_dict_tbg[this.test_colour];
@@ -213,14 +335,15 @@ class Memgame {
 
         return;
       } else {
-        console.log("Incorrect, this is not cosoco!");
+        this.answer_eval = "Incorrect, this is cosoco!";
+        console.log("Incorrect, this is cosoco!");
         this.wrong_answer.play();
         return;
       }
     } else {
       if (input == "right") {
+        this.answer_eval = "Correct, this is not a cosoco!";
         console.log("Correct, this is not a cosoco!");
-
         this.right_answer.play();
 
         // increase prob
@@ -229,7 +352,8 @@ class Memgame {
         }
         return;
       } else {
-        console.log("Incorrect, this is a cosoco!");
+        this.answer_eval = "Incorrect, this\nis not a cosoco!";
+        console.log("Incorrect, this is not a cosoco!");
         this.wrong_answer.play();
         return;
       }
--- memgame/sketch.js
+++ memgame/sketch.js
@@ -1,6 +1,8 @@
 var game;
 var n_cosocos;
 var round_finished_counter_set = false;
+var intro_made = false;
+var intro_counter = 0;
 
 let song, right_answer, wrong_answer;
 let polySynthesizer = new p5.PolySynth();
@@ -31,35 +33,50 @@ function draw() {
   right_answer.setVolume(slider.value());
   wrong_answer.setVolume(slider.value());
 
-    if (!clicked) {
-      textAlign(CENTER, 150);textSize(32);fill(color(0,0,0));text('VediSonor Memory Game', 200, 150);
-      textAlign(CENTER, CENTER);textSize(16);fill(color(0,0,0));text('Click to Start', 200, 200);
-    }
+  if (!clicked) {
+    textAlign(CENTER, 150);
+    textSize(32);
+    fill(color(0, 0, 0));
+    text('VediSonor Memory Game', 200, 150);
+    textAlign(CENTER, CENTER);
+    textSize(16);
+    fill(color(0, 0, 0));
+    text('Click to Start', 200, 200);
+  }
 
   // leftkey or say "yes" - its a cosoco
   // rightkey or say "no" - its not a cosoco
-  if (clicked){
-    if (!game.game_finished) {
-      game.play();
+  if (clicked) {
+
+    //explain rules
+    if (!intro_made) {
+      frameRate(8);
+      make_intro();
     } else {
 
-      if (!round_finished_counter_set) {
-        game.counter = millis();
-        round_finished_counter_set = true;
+      if (!game.game_finished) {
+        game.play();
+      } else {
 
-      }
+        if (!round_finished_counter_set) {
+          game.counter = millis();
+          round_finished_counter_set = true;
+        }
 
-      fill(color(0, 0, 0));
-      textSize(32);
-      text("Round finished!", 100, 200);
+        textAlign(CENTER, CENTER);
+        textSize(32);
+        fill(color(0, 0, 0));
+        text("Round finished!", 200, 150);
+        textSize(16);
+        fill(color(0, 0, 0));
+        text("Number of cosocos: " + game.n_cosocos + ".", 200, 200);
 
-      textSize(16);
-      text("Number of cosocos: " + game.n_cosocos + ".", 100, 220);
 
-      if (millis() >= 3000 + game.counter) {
-        n_cosocos++;
-        game = new Memgame(n_cosocos, polySynthesizer, right_answer, wrong_answer);
-        round_finished_counter_set = false;
+        if (millis() >= 3000 + game.counter) {
+          n_cosocos++;
+          game = new Memgame(n_cosocos, polySynthesizer, right_answer, wrong_answer);
+          round_finished_counter_set = false;
+        }
       }
     }
   }
@@ -69,4 +86,43 @@ function mouseClicked() {
   if (!clicked) {
     clicked = true;
   }
+}
+
+function make_intro() {
+
+  var text_arr = [
+    "Welcome to VediSonor, where\nyour ability to remember\nCOlour-SOund COmbinationS\n(cosocos) will be tested!",
+    "This game has multiple rounds.\nIn each round there are two stages:\nan introduction stage\nand a playing stage.",
+    "In the introduction stage\na set of cosocos will be given\nto you - remember them!",
+    "In the playing stage,\nyou will be tested on your\nability to remember cosocos.",
+    "Cosocos will be given to you\nthat may or may not have been\nthe ones that were given to you\nin the introduction.",
+    "If you think that the cosoco\nis part of the introduction set\nsay 'Yes', otherwise say 'No'.",
+    "At the end of the introduction,\nyou may repeat the set by saying\n'Repeat' or you can go\ninto the playing stage by\n saying 'Play'.",
+    "The first round will have 3 cosocos.\nThe subsequent rounds will\nhave +1 cosocos.",
+    "Please speak clearly and\nloudly for the voice commands.\nIf the command is not\nregistered, please wait\nand try again. Alternatively,\n you can use the arrow keys.",
+    "Are you ready? Click -> to continue!"
+  ];
+
+  textAlign(CENTER, CENTER);
+  textSize(24);
+  fill(color(0, 0, 0));
+  text(text_arr[intro_counter], 200, 150);
+  
+  textAlign(CENTER,BOTTOM);
+  textSize(16);
+  text("Left key (<-) to go back, Right key (->) to go foward", 200, 350);
+
+  if (keyIsDown(RIGHT_ARROW)) {
+    intro_counter++;
+  } else if (keyIsDown(LEFT_ARROW)) {
+    if (intro_counter > 0) {
+      intro_counter--;      
+    }
+  }
+
+  if (intro_counter == text_arr.length) {
+    frameRate(60);
+    intro_made = true;
+  }
+
 }
